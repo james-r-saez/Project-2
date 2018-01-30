db3c11b8c682b596534844fb8374b2a11deb1352
James R. Saez <31411325+james-r-saez@users.noreply.github.com>
2018-01-30 14:08:13 -0500

    Delete words.js

--- controllers/words.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const Words = require("../models/words.js");
-
-router.get("/", Words.allWords, (req, res, next) => {
-  res.render("words", { wordsData: res.locals.wordsData  });
-});
-
-module.exports = router;
